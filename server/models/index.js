a4b489f64fdce5459424d07a34a389ca1a44f5e4
Medo-ID <midaghdour@gmail.com>
2024-11-13 15:09:04 +0100

    add rejectUnauthorized to the pool

--- server/models/index.js
+++ server/models/index.js
@@ -6,8 +6,9 @@ const { NODE_ENV, DB_URL_PROD, DB_URL_DEV } = process.env;
 
 const { Pool } = pg;
 
-const connectionString = NODE_ENV === 'production' ? DB_URL_PROD : DB_URL_DEV;
- 
 export const pool = new Pool({
-  connectionString,
+  connectionString: NODE_ENV === 'production' ? DB_URL_PROD : DB_URL_DEV,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
