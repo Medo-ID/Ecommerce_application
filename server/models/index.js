02279506534bc6ebf830485c4862cf4387f38ccc
Medo-ID <midaghdour@gmail.com>
2024-11-14 13:10:39 +0100

    try neon database if it work in production

--- server/models/index.js
+++ server/models/index.js
@@ -6,7 +6,7 @@ dotenv.config();
 const { Pool } = pg;
 
 export const pool = new Pool({
-  connectionString: 'postgresql://ecom_restapi_db_owner:FJz8EaP5upiS@ep-withered-lab-a2pwtsm7.eu-central-1.aws.neon.tech/ecom_restapi_db?sslmode=require',
+  connectionString: process.env.DB_URL_PROD,
   ssl: {
     rejectUnauthorized: false
   }
