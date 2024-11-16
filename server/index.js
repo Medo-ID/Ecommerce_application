a03c2278c566675b06c2382a9af6ca2f1431d92b
Medo-ID <midaghdour@gmail.com>
2024-11-16 12:45:22 +0100

    try fix the cross-origin problem

--- server/index.js
+++ server/index.js
@@ -62,14 +62,6 @@ app.use(passport.session());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', FRONT_DOMAIN);
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-});
-
 // GitHub Authentication 2.0 Strategy
 // Config GitHubStrategy
 passport.use(new GitHubStrategy({
