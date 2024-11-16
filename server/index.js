c7a16fe4a7fb20c237b199ea40752b7bfc279ded
Medo-ID <midaghdour@gmail.com>
2024-11-16 14:09:15 +0100

    try to fix the status for auth user

--- server/index.js
+++ server/index.js
@@ -130,10 +130,6 @@ app.post('/create-checkout-session', async (req, res) => {
     }
 });
 
-app.get('/', (req, res) => {
-    console.log(req.isAuthenticated)
-})
-
 // APIs endpoint
 app.use('/api/auth', authRouter);
 app.use('/api/users', isAuthenticated, userRouter);
